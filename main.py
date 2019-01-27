17894ea90abb8580e25d613ee7dd8b1a108a5458
icevin <kevintang2023@gmail.com>
2019-01-27 02:28:12 -0500

    more anti anti false positive

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ class ScanMenu(QMainWindow, scanmenu.Ui_ScanMenu):
             symbols.append(LaundrySymbols.WASH_40)            
             symbols.append(LaundrySymbols.BLEACH_NOCL)
             symbols.append(LaundrySymbols.IRON_M)
-        elif(len(flag_BLEACH_NOCL) > 3 and len(flag_BLEACH_NOCL) < 14):
+        elif(len(flag_BLEACH_NOCL) > 3 and len(flag_BLEACH_NOCL) < 18):
             symbols.append(LaundrySymbols.WASH_30)            
             symbols.append(LaundrySymbols.BLEACH_NOCL)            
         return symbols
